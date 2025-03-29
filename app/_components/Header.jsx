b292db441d106e0bd5e1312a9c5969e7a16c7fe2
Saurabh <saurabhbhatt023@gmail.com>
2025-03-29 18:30:20 +0530

    Added the  login user

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -1,26 +1,39 @@
-"use client"
-import React from 'react';
-import Image from 'next/image';
-import { Button } from '@/components/ui/button';
-import Authentication from './Authentication';
+"use client";
+import React, { useContext } from "react";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import Authentication from "./Authentication";
+import { AuthContext } from "@/_context/AuthContext"; // Import the context
 
 const Header = () => {
+  const { user } = useContext(AuthContext); // Use useContext to access the context
+
   return (
-
-    <div className='p-4 flex items-center justify-between  '> 
-    <div className='flex items-center gap-3'>
-    <div>
-      <Image src="/logo.svg" alt="logo" width={30} height={40} />
-    </div>
-    <h2 className='text-2xl font-bold'> Clip Craft</h2>
-    </div>
-    <div> 
-        <Authentication>
-
-        <Button>Get Started</Button>
-        </Authentication>
-        
-    </div>
+    <div className="p-4 flex items-center justify-between">
+      <div className="flex items-center gap-3">
+        <div>
+          <Image src="/logo.svg" alt="logo" width={30} height={40} />
+        </div>
+        <h2 className="text-2xl font-bold">Clip Craft</h2>
+      </div>
+      <div>
+        {!user ? (
+          <Authentication>
+            <Button>Get Started</Button>
+          </Authentication>
+        ) : (
+          <div className="flex items-center gap-3">
+            <Button>Dashboard</Button>
+            <Image
+              src={user?.photoURL}
+              alt="userImage"
+              width={40}
+              height={40}
+              className="rounded-full"
+            />
+          </div>
+        )}
+      </div>
     </div>
   );
 };
--- app/provider.js
+++ app/provider.js
@@ -1,22 +1,40 @@
 "use client"
-import React from 'react'
+import React, { useEffect, useState, useContext } from 'react'
 import { ThemeProvider as NextThemesProvider } from "next-themes"
+import { onAuthStateChanged } from 'firebase/auth'
+import { AuthContext } from '@/_context/AuthContext'
+import { auth } from '@/configs/firebaseConfig'
 
-const Provider = ({children}) => {
+const Provider = ({ children }) => {
+  const [user, setUser] = useState() 
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      console.log(user);
+      setUser(user);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
   return (
-    <div>
-
-<NextThemesProvider   
-            attribute="class"
-            defaultTheme="dark"
-            enableSystem
-            disableTransitionOnChange>    
-    {children} 
-
-</NextThemesProvider>
-  
-    </div>
+    <AuthContext.Provider value={{user}}> 
+      <NextThemesProvider   
+        attribute="class"
+        defaultTheme="dark"
+        enableSystem
+        disableTransitionOnChange>    
+        {children} 
+      </NextThemesProvider>
+    </AuthContext.Provider>
   )
+}
+
+export const useAuthContext = () => {
+  const context = useContext(AuthContext)
+  if (context === undefined) {
+    throw new Error('useAuthContext must be used within an AuthProvider')
+  }
+  return context
 }
 
 export default Provider
