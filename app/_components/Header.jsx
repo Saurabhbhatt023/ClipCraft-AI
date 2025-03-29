c217aa8fe74eacab7cc15451bc60e482327cd3c1
Saurabh <saurabhbhatt023@gmail.com>
2025-03-29 12:36:31 +0530

    adding the ui

---
+++ app/_components/Header.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Image from 'next/image';
+import { Button } from '@/components/ui/button';
+
+const Header = () => {
+  return (
+
+    <div className='p-4 flex items-center justify-between  '> 
+    <div className='flex items-center gap-3'>
+    <div>
+      <Image src="/logo.svg" alt="logo" width={30} height={40} />
+    </div>
+    <h2 className='text-2xl font-bold'> Clip Craft</h2>
+    </div>
+    <div> 
+        <Button>Get Started</Button>
+    </div>
+    </div>
+  );
+};
+
+export default Header;
