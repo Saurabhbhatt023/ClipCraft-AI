f0acbfef3931d28d098d7ea1c95b0c1b7018444f
Saurabh <saurabhbhatt023@gmail.com>
2025-04-02 22:07:54 +0530

    Changes in image

--- app/(main)/_components/AppHeader.jsx
+++ app/(main)/_components/AppHeader.jsx
@@ -10,15 +10,7 @@ const AppHeader = () => {
   return (
     <div className="p-3 flex items-center space-x-3">
       
-      {user?.photoURL && (
-        <Image
-          src={user.photoURL}
-          alt="User Image"
-          width={40}
-          height={40}
-          className="rounded-full"
-        />
-      )}
+     
 
       <h1 className="text-lg font-bold">App Header</h1>
     </div>
